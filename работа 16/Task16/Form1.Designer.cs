6d7c20f80e30f0867f25949d3117545507633ddf
Coldstary <mihalinaleksandr2@gmail.com>
2025-06-19 21:08:20 +0300

    Add files via upload

---
+++ работа 16/Task16/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Task16
+{
+    partial class FrmTask16
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.AccessibleName = "txtbox";
+            this.txtInput.Location = new System.Drawing.Point(176, 162);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(206, 25);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.Tag = "txtbox";
+            this.txtInput.Visible = false;
+            this.txtInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(275, 235);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(135, 52);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(378, 162);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Ввод";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonInput_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(315, 104);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(48, 16);
+            this.lbl.TabIndex = 7;
+            this.lbl.Text = "LABEL";
+            this.lbl.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // FrmTask16
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 413);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtInput);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmTask16";
+            this.Text = "FrmTask16";
+            this.Load += new System.EventHandler(this.TextBox1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbl;
+    }
+}
+
---
+++ работа 16/Task16/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Task16
+{
+    public partial class FrmTask16 : Form
+    {
+        public FrmTask16()
+        {
+          
+                InitializeComponent();
+            }
+
+            private void buttonInput_Click(object sender, EventArgs e)
+            {
+                // Проверка видимости поля ввода
+                if (txtInput.Visible == false)
+                {
+                    // Показываем поле ввода при первом нажатии
+                    txtInput.Visible = true;
+                    txtInput.Text = "";
+                    txtInput.Focus();  // Устанавливаем фокус ввода
+                }
+                else
+                {
+                    // Обработка введенного текста
+                    string inputText = txtInput.Text.Trim();
+
+                    if (string.IsNullOrEmpty(inputText))
+                    {
+                        // Показ предупреждения при пустом вводе
+                        MessageBox.Show("Поле ввода не может быть пустым.",
+                                       "Ошибка ввода",
+                                       MessageBoxButtons.OK,
+                                       MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        // Установка введенного текста в textBox2
+                        lbl.Text = inputText;
+                    }
+                }
+            }
+
+            private void buttonClear_Click(object sender, EventArgs e)
+            {
+                // Проверка содержимого textBox2
+                if (string.IsNullOrEmpty(lbl.Text))
+                {
+                    // Сообщение, если поле уже пустое
+                    MessageBox.Show("Ярлык уже пуст.",
+                                   "Очистка",
+                                   MessageBoxButtons.OK,
+                                   MessageBoxIcon.Information);
+                }
+                else
+                {
+                    // Очистка textBox2
+                   lbl.Text = "";
+                }
+            }
+
+            // Остальные методы остаются без изменений
+            private void label1_Click(object sender, EventArgs e) { }
+            private void textBox1_TextChanged(object sender, EventArgs e) { }
+            private void TextBox1_Load(object sender, EventArgs e) { }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+    }
